1eaf49385011c36efdfa39f53227dd72b23f2169
Viktor Danov <orangeleaf12@gmail.com>
2024-02-06 20:19:05 +0200

    chore: update deps

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,14 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"math/rand"
 	"os"
 	"runtime/pprof"
-
-	. "github.com/viktordanov/lsystem"
 )
 
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
-	// seed random number generator
-	rand.Seed(1)
-
 	flag.Parse()
 	if *cpuprofile != "" {
 		f, err := os.Create(*cpuprofile)
@@ -26,20 +19,4 @@ func main() {
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
 	}
-	rulesStr := map[Token]string{
-		"A":  "1 A; 1 A B C6",
-		"C5": "1 x",
-		"B":  "1 o; 1 x",
-	}
-
-	vars, consts, rules := ParseRules(rulesStr)
-	lsys := NewLSystem("A", rules, vars, consts, true)
-
-	lsys.Reset()
-	fmt.Println(lsys.DecodeBytes(lsys.MemPool.ReadAll()))
-	for i := 0; i < 10; i++ {
-		fmt.Println(lsys.DecodeBytes(lsys.IterateOnce()))
-	}
-
-	fmt.Println(lsys)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,14 @@ module github.com/viktordanov/lsystem
 go 1.21.3
 
 require (
+	github.com/go-echarts/go-echarts v1.0.0
+	github.com/go-echarts/go-echarts/v2 v2.3.2
+	github.com/stretchr/testify v1.8.4
+	pgregory.net/rand v1.0.2
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	pgregory.net/rand v1.0.2 // indirect
 )
